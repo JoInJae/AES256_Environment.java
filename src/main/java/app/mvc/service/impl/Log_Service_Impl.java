2a4e0c4536e69ff49ff945da1219664f5486e9a7
JoInJae <andinjai@naver.com>
2021-09-15 18:50:30 +0900

    commit 인재

--- src/main/java/app/mvc/service/impl/Log_Service_Impl.java
+++ src/main/java/app/mvc/service/impl/Log_Service_Impl.java
@@ -18,7 +18,6 @@ import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 @Service
